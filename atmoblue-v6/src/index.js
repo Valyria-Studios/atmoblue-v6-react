435894e6d073976b38f3772fdd0f8780175c853e
Noah B <79390298+dajpkov@users.noreply.github.com>
2023-12-16 19:17:24 -0500

    add firebase config

--- atmoblue-v6/src/index.js
+++ atmoblue-v6/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+import { collection, getDocs } from 'firebase/firestore';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
